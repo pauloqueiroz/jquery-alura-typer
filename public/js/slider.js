402bb1534c74b76916418edd2660294abfbc7ca7
Paulo Sérgio <paulosergio@infoway-pi.com.br>
2020-01-23 14:12:34 -0300

    Implementação de slider usando biblioteca slick

---
+++ public/js/slider.js
@@ -0,0 +1,12 @@
+$(function(){
+	console.log("opa");
+	$(".slider").slick(
+	{
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    }
+    );
+});
